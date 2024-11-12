197c18d5644f2f71ef5e76dedb4b384cb7dd32af
efirenetd <efirenetd@gmail.com>
2024-11-12 16:48:41 +0800

    added README

---
+++ src/main/java/ph/com/pbcom/T24DataValidationApplication.java
@@ -0,0 +1,13 @@
+package ph.com.pbcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T24DataValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T24DataValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ph/com/pbcom/T24DataValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.com.pbcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T24DataValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
