499f8e807323971af343c4df8e46807482bff488
efirenetd <efirenetd@gmail.com>
2024-11-15 13:06:00 +0800

    used JpaTransactionManager instead of DataSourceTransactionManager (ref stackoverlow: https://stackoverflow.com/questions/72954847/spring-batch-repositoryitemwriter-not-working)

--- src/main/java/ph/com/pbcom/config/BatchConfig.java
+++ src/main/java/ph/com/pbcom/config/BatchConfig.java
@@ -1,13 +1,9 @@
 package ph.com.pbcom.config;
 
-import org.springframework.batch.core.explore.JobExplorer;
-import org.springframework.batch.core.explore.support.JobExplorerFactoryBean;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
@@ -23,7 +19,7 @@ public class BatchConfig {
 
     @Bean
     public PlatformTransactionManager transactionManager(){
-        return new DataSourceTransactionManager(dataSource);
+        return new JpaTransactionManager();
     }
 
 }
